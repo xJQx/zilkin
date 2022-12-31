1c9c617fed554a1f64633b575bd1255e15a7b656
Jing Qiang <tohjingqiang@gmail.com>
2022-12-31 22:49:48 +0800

    update navbar links

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,20 +3,23 @@ import { Logo } from './Logo';
 import { NavMenu } from './NavMenu';
 
 export const Navbar = () => {
-  const links = [
-    { label: 'Wallet', href: '/wallet' },
-    { label: 'Portal', href: '/portal' },
-    { label: 'Blog', href: '/blog' },
-    { label: 'Discord', href: '/discord' },
-  ];
-
   return (
     <div
       id="nav-container"
       className="fixed flex w-full px-6 py-4 justify-between bg-transparent backdrop-blur"
     >
       <Logo />
-      <NavMenu links={links} />
+      <NavMenu
+        links={[
+          { label: 'Home', href: '/' },
+          { label: 'Contracts', href: '/contracts' },
+          { label: 'Deploy', href: '/deploy' },
+          {
+            label: 'Scilla Docs',
+            href: 'https://scilla.readthedocs.io/en/latest/',
+          },
+        ]}
+      />
     </div>
   );
 };
