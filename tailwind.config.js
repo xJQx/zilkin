fe46cbd8cf1f08373f775cc3ff68d11bb841b398
Jing Qiang <tohjingqiang@gmail.com>
2022-12-31 13:16:47 +0800

    initialise\ tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: 'class',
+};
