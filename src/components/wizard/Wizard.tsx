5ee05360c369337aa430105a1934ca983762ad39
Jing Qiang <tohjingqiang@gmail.com>
2023-01-03 22:30:54 +0800

    add zrc2 configurations + wizardCodeBody

--- src/components/wizard/Wizard.tsx
+++ src/components/wizard/Wizard.tsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { ContractType } from '../../lib/types';
 import { ContractButton } from './ContractButton';
 import { FaCopy } from 'react-icons/fa';
+import { Zrc2Config } from './config';
 
 export const Wizard = () => {
   const [selectedContract, setSelectedContract] =
     useState<ContractType>('zrc2');
+  const [wizardCodeBody, setWizardCodeBody] = useState('');
 
   // When user change the scilla contract that they want
   useEffect(() => {
@@ -21,11 +23,11 @@ export const Wizard = () => {
     <div className="flex justify-center items-center pt-40 px-6">
       <div
         id="wizard-container"
-        className="container flex flex-col p-4 min-h-[52rem] max-w-[1200px] bg-slate-200 rounded-lg"
+        className="container flex flex-col p-4 min-h-[42rem] max-w-[1200px] bg-slate-200 rounded-lg overflow-x-auto"
       >
         <div
           id="wizard-header"
-          className="flex flex-row justify-between gap-2 items-center font-bold overflow-x-auto pb-4"
+          className="flex flex-row justify-between gap-2 items-center font-bold overflow-x-auto pb-4 min-w-[24rem]"
         >
           <div id="wizard-contracts-type" className="flex gap-2">
             <ContractButton
@@ -51,20 +53,22 @@ export const Wizard = () => {
             </button>
           </div>
         </div>
-        <div id="wizard-body" className="flex flex-row gap-4 grow">
+        <div
+          id="wizard-body"
+          className="flex flex-row gap-4 grow min-w-[24rem]"
+        >
           <div
             id="wizard-configuration"
             className="w-64 bg-slate-50 rounded-lg p-4 shadow"
           >
-            <div>Options</div>
-            <div>Features</div>
-            <div>Access Control</div>
-            <div>Info</div>
+            <Zrc2Config setWizardCodeBody={setWizardCodeBody} />
           </div>
           <div
             id="wizard-code"
-            className="w-full bg-gray-800 rounded-lg shadow"
-          ></div>
+            className="w-full max-h-[42rem] bg-gray-800 rounded-lg shadow text-white overflow-auto p-4"
+          >
+            <pre>{wizardCodeBody}</pre>
+          </div>
         </div>
       </div>
     </div>
