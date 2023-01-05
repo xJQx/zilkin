d4ed44eaab1c6891d14c89752bad304ab6ba8d9b
Jing Hua <tohjinghua123@gmail.com>
2023-01-05 19:49:02 +0800

    fix navbar link

--- src/components/navbar/NavLink.tsx
+++ src/components/navbar/NavLink.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export interface NavLinkProps {
   label: string;
@@ -9,11 +10,11 @@ export const NavLink = (props: NavLinkProps) => {
   const { label, href } = props;
 
   return (
-    <a
-      href={href}
+    <Link
+      to={href}
       className="px-3 py-2 rounded-2xl text-lg text-center hover:text-white hover:bg-brand-blue-dark w-52 lg:w-auto"
     >
       {label}
-    </a>
+    </Link>
   );
 };
